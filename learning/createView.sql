b1ba13b513d3612ff468081da6ef7b18da8d30d5
Somadina Mbadiwe <somasystemsng@gmail.com>
2018-11-27 21:32:23 -0500

    Fixed createView query that causes infinite runs in the code

--- learning/createView.sql
+++ learning/createView.sql
@@ -3,4 +3,4 @@ create view labeled_data as
 	from gh_so_common_users c
 	union
 	select c.gh_user_id as g_id, c.so_user_id as s_id, 0 as label 
-	from users g, so_users s, negative_user_pairs c
+	from negative_user_pairs c
